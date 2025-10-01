912c4962043d00a213b8e1336892ae7fceb551ba
Supravisor <153783117+Supravisor@users.noreply.github.com>
2025-10-02 12:36:33 +1300

    add canvasCalcs

--- public/game.mjs
+++ public/game.mjs
@@ -12,6 +12,17 @@ const playerHeight = 30;
 const border = 5; // Between edge of canvas and play field
 const infoBar = 45; 
 
+const canvasCalcs = {
+  canvasWidth: canvasWidth,
+  canvasHeight: canvasHeight,
+  playFieldMinX: (canvasWidth / 2) - (canvasWidth - 10) / 2,
+  playFieldMinY: (canvasHeight / 2) - (canvasHeight - 100) / 2,
+  playFieldWidth: canvasWidth - (border * 2),
+  playFieldHeight: (canvasHeight - infoBar) - (border * 2),
+  playFieldMaxX: (canvasWidth - playerWidth) - border,
+  playFieldMaxY: (canvasHeight - playerHeight) - border,
+}
+
 const loadImage = src => {
   const img = new Image();
   img.src = src;
