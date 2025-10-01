11e9061208c504bd4ebc12cfcec7a1bf41c31ab8
Supravisor <153783117+Supravisor@users.noreply.github.com>
2025-10-02 12:33:55 +1300

    add canvas variables

--- public/game.mjs
+++ public/game.mjs
@@ -5,6 +5,13 @@ const socket = io();
 const canvas = document.getElementById('game-window');
 const context = canvas.getContext('2d');
 
+const canvasWidth = 640;
+const canvasHeight = 480;
+const playerWidth = 30;
+const playerHeight = 30;
+const border = 5; // Between edge of canvas and play field
+const infoBar = 45; 
+
 const loadImage = src => {
   const img = new Image();
   img.src = src;
