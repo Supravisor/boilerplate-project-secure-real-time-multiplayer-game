3d5360dfbcf3ea0e8835741f38c894d283b782b7
Supravisor <153783117+Supravisor@users.noreply.github.com>
2025-10-01 16:29:08 +1300

    load image

--- public/game.mjs
+++ public/game.mjs
@@ -4,3 +4,9 @@ import Collectible from './Collectible.mjs';
 const socket = io();
 const canvas = document.getElementById('game-window');
 const context = canvas.getContext('2d');
+
+const loadImage = src => {
+  const img = new Image();
+  img.src = src;
+  return img;
+}
